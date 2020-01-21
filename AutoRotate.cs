f52ddcb3f0a738544e7957e9afbfcbcefa10e711
sanpeita <50853225+sanpeita@users.noreply.github.com>
2020-01-21 16:32:15 +0900

    Add files via upload

---
+++ AutoRotate.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AutoRotate : MonoBehaviour
+{
+    
+    public float Rot_x;
+    public float Rot_y;
+    public float Rot_z;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(Rot_x,Rot_y,Rot_z);
+    }
+}
