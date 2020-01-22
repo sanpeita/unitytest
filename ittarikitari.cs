5d333e7582d78c2f7802f42032b956da5e53b19a
sanpeita <50853225+sanpeita@users.noreply.github.com>
2020-01-22 13:15:45 +0900

    Add files via upload

---
+++ ittarikitari.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class ittarikitari : MonoBehaviour
+{
+    [SerializeField]
+    private Vector3 _startPosition;
+
+    [SerializeField]
+    private Vector3 _targetPosition;
+
+    [SerializeField]
+    private float _duration = 1f;
+
+    private float _time = 0;
+    //private int _dirFactor = 1;
+    private bool _inverse = false;
+
+    private void Update()
+    {
+        _time += Time.deltaTime;
+
+        // 指定時間を過ぎたら向きを逆に
+        if (_time >= _duration)
+        {
+            _time = 0;
+            _inverse = !_inverse;
+        }
+
+        // 時間を媒介変数として計算（0 - 1）
+        float t = _time / _duration;
+
+        if (_inverse)
+        {
+            transform.position = Vector3.Lerp(_startPosition, _targetPosition, 1f - t);
+        }
+        else
+        {
+            transform.position = Vector3.Lerp(_startPosition, _targetPosition, t);
+        }
+    }
+}
